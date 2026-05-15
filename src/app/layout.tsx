a181f2e0a37d95758ecc0c8fd6cb5722f3aa8648
user <me@example.com>
2026-05-15 19:13:17 +0530

    footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar/page";
+import Footer from "@/components/footer/page";
 
 
 
@@ -29,7 +31,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col"><Navbar />{children}<Footer /></body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,6 @@
-import Footer from "./footer/page";
-import Navbar from "./navbar/page";
-import "./globals.css";
-
 function page() {
   return (
     <div>
-    <Navbar />
-    <Footer /> 
     </div>
   );
 };
